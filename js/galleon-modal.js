1e013bfab687e47b9e293946f0801f87a89ee345
FabioMonzani <renderbros@gmail.com>
2021-03-22 09:48:50 +0000

    Defiville pirate radio deploy

---
+++ js/galleon-modal.js
@@ -0,0 +1,11 @@
+function galleonModal(e) {
+  document.getElementById('btn-click').play();
+  // var galleonType = $(e).attr('class');
+  // console.log(galleonType);
+  // if (galleonType == 'pirate-radio') {
+    $('#pirateRadioModal').modal();
+  // } else if (galleonType == 'ghost-galleon') {
+  //   $('#pirateRadioModal').modal();
+    // $('#ghostGalleonModal').modal();
+  // }
+}
