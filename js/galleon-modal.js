e80b57c9d957eefe47cbd23066db56b07ff86cbc
FabioMonzani <renderbros@gmail.com>
2021-04-13 01:10:13 +0100

    Fresh start

--- js/galleon-modal.js
+++
@@ -1,9 +0,0 @@
-function galleonModal(e) {
-  var galleonType = $(e).attr('class');
-  console.log(galleonType);
-  if (galleonType == 'pirate-radio') {
-    $('#pirateRadioModal').modal();
-  } else if (galleonType == 'ghost-galleon') {
-    $('#ghostGalleonModal').modal();
-  }
-}
